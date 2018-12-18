e01ea6ea01de8fea550ff226c95ff69d210a206a
HebertCL <hebert.cuellar@gmail.com>
2018-12-18 09:47:23 -0600

    Refactor aws_ec2

--- aws_ec2/src/main.go
+++ aws_ec2/src/main.go
@@ -2,33 +2,36 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"fmt"
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/ec2"
+  "fmt"
 )
 
 func main() {
-	// Session
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1")},
-	)
-	// Create EC2 service client
-	svc := ec2.New(sess)
+  createEc2Instance("ami-07c20c8dd8c3136bf", "t2.micro", 1, 1)
+}
 
-	// Specify the details of the instance that you want to create
-	runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
-	// An Amazon Linux AMI ID for t2.micro instances
-		ImageId: 	aws.String("ami-07c20c8dd8c3136bf"),
-		InstanceType:	aws.String("t2.micro"),
-		MinCount:	aws.Int64(1),
-		MaxCount:	aws.Int64(1),
-	})
-
-	if err != nil {
-		fmt.Println("Could not create instance :(", err)
-		return
-	}
-
-	fmt.Println("Created instance!", *runResult.Instances[0].InstanceId)
+func createEc2Instance(instanceId string, instanceType string, minCount int, maxCount int) (result string, err error) {
+  // Creates a new session
+  sess, err := session.NewSession(&aws.Config{
+    Region: aws.String("us-east-1")
+  },
+  if err != nil {
+    return err
+  }
+  // Returns connection error, else connects to EC2 service
+  svc := ec2.New(sess)
+  // Specify instance details
+  runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+    ImageId:       aws.String(imageId),
+    InstanceType:  aws.String(instanceType),
+    MinCount:      aws.Int64(minCount),
+    MaxCount:      aws.Int64(maxCount), 
+  })
+  if err != nil {
+    return err
+  }
+  // Returns runInstance error, else returns created instance ID
+  return *runResult.Instances[0].InstanceId
 }
